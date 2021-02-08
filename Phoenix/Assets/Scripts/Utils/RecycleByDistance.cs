c2de09443d35a1d984c249938c40805e984b66bb
finalspace <finalspace.1031@gmail.com>
2021-02-07 20:33:05 -0800

    map bg tile recycle

---
+++ Phoenix/Assets/Scripts/Utils/RecycleByDistance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecycleByDistance : MonoBehaviour
+{
+    public Transform refRoot;
+    public float distance;
+
+    private void Awake()
+    {
+        if (refRoot == null)
+        {
+            refRoot = CameraManager.Instance.transform;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(refRoot.position, transform.position) > distance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
