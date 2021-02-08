386aedbb732de31329b0a73033cbbf98618e5b7c
finalspace <finalspace.1031@gmail.com>
2021-02-07 22:21:13 -0800

    random background tile item

---
+++ Phoenix/Assets/Scripts/Level/BackgroundTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject[] spots;
+
+    public void Start()
+    {
+        Spawn();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        for (int i = 0; i < spots.Length; i++)
+        {
+            float percentage = 1.0f / (spots[i].transform.childCount * 2);
+            for (int j = 0; j < spots[i].transform.childCount; j++)
+            {
+                if (RandomGenerator.Test(percentage))
+                {
+                    spots[i].transform.GetChild(j).gameObject.SetActive(true);
+                    break;
+                }
+            }
+
+        }
+    }
+}
